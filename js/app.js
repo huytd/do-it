1fc0d8ec4231fdc5732871970b4e7ecdb01f450d
Huy Tran <me864@Huys-MacBook-Pro.local>
2015-01-14 16:13:25 +0700

    Add loader for loading resource with loading bar

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ app.run(function(){
   $(".task-list").show();
 });
 
-app.controller('doitController', function($scope, $localStorage){
+app.controller('doitController', function($scope, $localStorage, $q){
 
   $scope.today = "D" + (new Date()).getDate() + "" + (new Date()).getMonth() + "" + (new Date()).getFullYear();
   $scope.todayID = 0;
@@ -13,15 +13,19 @@ app.controller('doitController', function($scope, $localStorage){
   $scope.currentTimeValue = (new Date()).getHours() * 100 + (new Date()).getMinutes() - 15;
 
   $scope.loadSavedData = function() {
+    var func = $q.defer();
     $scope.tasks = $localStorage.tasks;
     for (var i = 0; i < $scope.tasks.length; i++) {
       if ($scope.tasks[i].id == $scope.today) {
         $scope.todayID = i; console.log("Today ID: " + $scope.todayID);
       }
     }
+    func.resolve();
+    return func.promise;
   }
 
   $scope.initNewData = function() {
+    var func = $q.defer();
     $scope.tasks = [
     {
       id: $scope.today,
@@ -44,6 +48,8 @@ app.controller('doitController', function($scope, $localStorage){
         };
       }
     }
+    func.resolve();
+    return func.promise;
   }
 
   $scope.saveData = function() {
@@ -55,20 +61,19 @@ app.controller('doitController', function($scope, $localStorage){
   // Startup
 
   if (angular.isDefined($localStorage.tasks)) {
-    $scope.loadSavedData();
+    $scope.loadSavedData().then(function(){ finished() });
   } else {
-    $scope.initNewData();
+    $scope.initNewData().then(function(){ finished() });
   }
 
-  $(document).ready(function(){
+  function finished() {
+    console.log("Run done");
     setTimeout(function(){
+      $(".available:eq(0)").addClass("highlight");
       $('html, body').animate({
         scrollTop: $(".available:eq(0)").offset().top - $(".available:eq(0)").outerHeight()
       }, 1000);
     }, 300);
-    $(".available:eq(0)").addClass("highlight");
-
-    $(".loading-bar").css("width", "100%").delay(1000).fadeOut();
-  });
+  }
 
 });
